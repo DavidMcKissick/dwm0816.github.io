d13f19aa6a7417b428f4373655be16521cef3466
dwm0816 <dwm0816@gmail.com>
2018-03-04 20:15:47 -0800

    Major mobile overhaul

---
+++ mobile/jsjq/news.js
@@ -0,0 +1,9 @@
+$('#news').append('<h4 class="storyTitle">Gearing up.</hr>')
+$('#news').append('<p class="snippet">After months of preparation, we are nearly ready to launch!</p>')
+$('#news').append('<a href="news/22518.html" class="readMore">Read more...</p>')
+
+$('#news').append('<hr>')
+
+$('#news').append('<h4 class="storyTitle">A brand new website!</hr>')
+$('#news').append('<p class="snippet">We have officially launched a new website!</p>')
+$('#news').append('<a href="news/81517.html" class="readMore">Read more...</p>')
---
+++ mobile/news/newsapp.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $("[href]").each(function () {
+        if (this.href == window.location.href) {
+            $(this).addClass("currentLink");
+        }
+    });
+});
+
+$('#core').prepend('<div class="header">')
+$('.header').append('<div class="logohead">')
+$('.logohead').append('<a href="../index.html"><h1 id="headtext">SKYBROOK</h1></a>')
+$('.header').append('</div>')
+$('.header').append('<div id="spacer"></div>')
+$('.logohead').append('<div class="menu">')
+$('.menu').append('<div class="mainmenu">')
+$('.mainmenu').append('<ul id="menulist">')
+$('#menulist').append('<x id="menuitem"><a href="../index.html">Home</a>', '</x>')
+$('#menulist').append('<x class="menuSep">|</x>')
+$('#menulist').append('<x id="menuitem"><a href="../resources.html">Resources</a>', '</x>')
+$('#menulist').append('<x class="menuSep">|</x>')
+$('#menulist').append('<x id="menuitem"><a href="../about.html">About</a>', '</x>')
+$('.mainmenu').append('</ul>')
+$('.menu').append('</div>')
+$('.header').append('</div>')
+
+
+// End of Header
+
+// Footer
+$('#core').append('<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>')
+$('#core').append('<div id="footer">')
+$('#footer').append('<div id="footContent">')
+$('#footContent').append('<p id="copyrightNotice">All content is wholly owned by Skybrook Foundation, copyright 2018©<br><br>DISCLAIMER: While we attempt to keep information as up to date and accurate as possible, we are not responsible for accidentally inaccurate information. If you have questions make sure to inquire further with your physician. If you spot an inaccuracy in our information, feel free to let us know via the contact us form.</p>')
+$('#footer').append('</div>')
+$('#core').append('</div>')
